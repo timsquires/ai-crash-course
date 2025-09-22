4d3878b7a88219df54229b8837b74c42da96376f
Nick Hotalling <nick.hotalling@flightschedulepro.com>
2025-09-22 13:44:20 -0500

    fix api runtime errors

--- apps/api/src/persistence/models.module.ts
+++ apps/api/src/persistence/models.module.ts
@@ -2,20 +2,18 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ThreadEntity } from '../threads/infra/postgres/thread.entity';
-import { ThreadModel, ThreadSchema } from '../threads/infra/mongo/thread.schema';
+import {
+  ThreadModel,
+  ThreadSchema,
+} from '../threads/infra/mongo/thread.schema';
 
 @Module({
   imports: [
-    ...(process.env.PERSISTENCE === 'postgres' ? [TypeOrmModule.forFeature([ThreadEntity])] : []),
-    ...(process.env.PERSISTENCE === 'mongo'
-      ? [MongooseModule.forFeature([{ name: ThreadModel.name, schema: ThreadSchema }])]
-      : []),
+    TypeOrmModule.forFeature([ThreadEntity]),
+    MongooseModule.forFeature([
+      { name: ThreadModel.name, schema: ThreadSchema },
+    ]),
   ],
-  exports: [
-    ...(process.env.PERSISTENCE === 'postgres' ? [TypeOrmModule] : []),
-    ...(process.env.PERSISTENCE === 'mongo' ? [MongooseModule] : []),
-  ],
+  exports: [TypeOrmModule, MongooseModule],
 })
 export class ModelsModule {}
-
-
