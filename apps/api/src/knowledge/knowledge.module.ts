bd845e1d9fe581afdddc960eefdd0b0b7d269579
Tim <tim.squires@flightschedulepro.com>
2025-10-01 15:17:44 -0500

    Change chunking strategy

--- apps/api/src/knowledge/knowledge.module.ts
+++ apps/api/src/knowledge/knowledge.module.ts
@@ -3,7 +3,7 @@ import { RepositoryModule } from '../persistence/repository.module';
 import { KnowledgeService } from './knowledge.service';
 import { KnowledgeController } from './knowledge.controller';
 import { SlidingWindowChunker } from './chunking/sliding-window.chunker';
-import { RestaurantSectionChunker } from './chunking/restaurant-section.chunker';
+// import { RestaurantSectionChunker } from './chunking/restaurant-section.chunker';
 import { CHUNKING_STRATEGY } from './chunking/chunking.strategy';
 import { DocumentLoaderService } from './document-loader.service';
 import { EmbeddingsService } from './embeddings.service';
@@ -16,7 +16,8 @@ import { EmbeddingsService } from './embeddings.service';
     DocumentLoaderService,
     EmbeddingsService,
     // Replace the useClass value with a different chunking strategy if you want to use a different one
-    { provide: CHUNKING_STRATEGY, useClass: RestaurantSectionChunker },
+    // { provide: CHUNKING_STRATEGY, useClass: RestaurantSectionChunker },
+    { provide: CHUNKING_STRATEGY, useClass: SlidingWindowChunker },
   ],
   exports: [KnowledgeService, CHUNKING_STRATEGY],
 })
